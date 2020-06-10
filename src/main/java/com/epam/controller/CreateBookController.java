82da5e32f5f0e276ef9822d1482bbddfd3bfccba
danila16030 <danila.chechitov1603@gmail.com>
2020-06-10 17:28:56 +0300

    add mapping in controller layer

--- src/main/java/com/epam/controller/CreateBookController.java
+++ src/main/java/com/epam/controller/CreateBookController.java
@@ -3,19 +3,18 @@ package com.epam.controller;
 import com.epam.dto.BookDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.test.web.ModelAndViewAssert;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping(value = "book")
+@Controller
+@RestController(value = "book")
 public class CreateBookController {
-    @GetMapping("/view")
+    @RequestMapping(value = "view", method = RequestMethod.GET)
     public BookDTO createNewBook() {
         BookDTO bookDTO = new BookDTO();
-        bookDTO.setAuthor("films");
+        bookDTO.setAuthor("FF");
         System.out.println("ffff");
         return bookDTO;
     }
