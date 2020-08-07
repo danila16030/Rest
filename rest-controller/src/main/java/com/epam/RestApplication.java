8f089deb9ba2a7ac22fbb7dd7ee1f86e3813dbd1
danila16030 <danila.chechitov1603@gmail.com>
2020-08-07 21:02:01 +0300

    code refactoring

--- rest-controller/src/main/java/com/epam/RestApplication.java
+++ rest-controller/src/main/java/com/epam/RestApplication.java
@@ -1,12 +1,13 @@
 package com.epam;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
-public class RestApplication {
+public class RestApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        run(RestApplication.class, args);
+        SpringApplication.run(RestApplication.class, args);
     }
 }
